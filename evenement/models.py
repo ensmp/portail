3b45f700415e5ce3937043b6d4d9dba1b67310e6
unknown <Etienne@Etienne-TOSH.(none)>
2012-06-15 18:19:09 +0200

    Ajout de la billetterie (models)

--- evenement/models.py
+++ evenement/models.py
@@ -9,7 +9,26 @@ class Evenement(models.Model):
 	titre = models.CharField(max_length=300)
 	description =  models.TextField()
 	date = models.DateTimeField(default=datetime.now(), blank=True)
+	lieu = models.CharField(max_length=300)
 	participants = models.ManyToManyField(UserProfile,related_name='evenement_participant', blank=True)
 	
 	def __unicode__(self):
 		return self.titre
+		
+class Billetterie(models.Model):
+	evenement = models.ForeignKey(Evenement)
+	prix = models.IntegerField()	
+	reservations = models.ManyToManyField(Reservation,related_name='+', blank=True)
+	date_fin_reservation = models.DateTimeField()
+	nombre_places_total = models.IntegerField()
+	
+	def __unicode__(self):
+		return self.evenement.titre
+	
+class Reservation(models.Model):
+	eleve = models.ForeignKey(UserProfile)
+	acceptee = models.BooleanField()
+	
+	def __unicode__(self):
+		return self.titre
+	
