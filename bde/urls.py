0e5e66b7abc523ed413feaf71164438a6537d3d4
Guillaume Caner <guillaume.caner@mines-paristech.fr>
2013-09-07 13:37:49 +0200

    Insertion onglet Palum dans BDE

--- bde/urls.py
+++ bde/urls.py
@@ -5,4 +5,6 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('bde.views',
     url(r'^archives_palum/json/$', 'archives_palum', name='archives_palum_json'),
     url(r'^archives_palum/$', 'archives_palum', name='archives_palum'),
+    url(r'^voter/$', 'voter', name='voter'),
+    url(r'^resultats/$', 'resultats', name='resultats'),
   )
