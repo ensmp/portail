df5fc384c2a2f3e198a5a522407ed6973e58e01f
Edouard Leurent <eleurent@gmail.com>
2013-11-21 11:11:12 +0100

    Refactoring models association

--- association/models.py
+++ association/models.py
@@ -1,25 +1,21 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-from trombi.models import UserProfile
 from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.models import Group, User
-from datetime import datetime
-from notification.models import Notification, Envoi
 from django.contrib.contenttypes import generic
+from datetime import datetime
+from trombi.models import UserProfile
+from notification.models import Notification, Envoi
 
-
-
-
-#Une association, comme ensemble de membres
 class Association(models.Model):
     nom = models.CharField(max_length=200)
-    pseudo = models.SlugField()
-    groupe_permissions = models.OneToOneField(Group, blank=True, null=True)
+    pseudo = models.SlugField(help_text="Nom dans les urls")
     membres = models.ManyToManyField(UserProfile, through='Adhesion', blank=True, null=True)
-    suivi_par= models.ManyToManyField(User, related_name='associations_suivies', blank=True, null=True)    
-    ordre = models.IntegerField(default=0)
-    is_hidden_1A = models.BooleanField(default=False,verbose_name="Caché aux 1A")
+    is_hidden_1A = models.BooleanField(default=False, verbose_name="Cachée aux 1A")
+    ordre = models.IntegerField(default=0, help_text="Ordre d'apparition dans la liste des associations (ordre alphabétique pour les valeurs égales)")
+    suivi_par = models.ManyToManyField(User, related_name='associations_suivies', blank=True, null=True, help_text= "Les élèves recevant les notifications de l'association")    
+    groupe_permissions = models.OneToOneField(Group, blank=True, null=True, help_text="Groupe de permissions correspondant à l'association")
  
     class Meta:
         ordering = ['ordre','nom']
@@ -31,7 +27,7 @@ class Association(models.Model):
         return self.is_hidden_1A and eleve.en_premiere_annee()
 
     def save(self, *args, **kwargs):
-        if not self.groupe_permissions: #On cree un groupe de permissions, si non-existant
+        if not self.groupe_permissions: # On crée un groupe de permissions, si non-existant
             groupe = Group.objects.create(name=self.nom)
             self.groupe_permissions = groupe
         super(Association, self).save(*args, **kwargs) # Sauvegarde
@@ -39,12 +35,15 @@ class Association(models.Model):
     def get_absolute_url(self):
         return '/associations/' + self.pseudo + '/'
 
-#Permet de préciser l'appartenance d'un membre à une assoce, et en particulier son rôle.
 class Adhesion(models.Model):
-    eleve = models.ForeignKey(UserProfile)
+    """
+        Permet de préciser l'appartenance d'un membre 
+        à une assoce, et en particulier son rôle.
+    """
+    eleve = models.ForeignKey(UserProfile, verbose_name="élève")
     association = models.ForeignKey(Association)
-    role = models.CharField(max_length=64, blank=True) #role dans l'association
-    ordre = models.IntegerField(default = 0) #ordre d'apparition dans la page 'equipe' de l'assoce
+    role = models.CharField(max_length=64, blank=True, help_text="Rôle dans l'association")
+    ordre = models.IntegerField(default = 0, help_text="Ordre décroissant d'apparition de l'élève dans la page 'équipe' de l'assoce")
     
     class Meta:
         ordering = ['-ordre']
@@ -53,26 +52,29 @@ class Adhesion(models.Model):
         return self.eleve.user.username + ' -> ' + self.association.nom
 
     def save(self, *args, **kwargs):
-        self.association.groupe_permissions.user_set.add(self.eleve.user) #On ajoute l'eleve au groupe de permissions
+        self.association.groupe_permissions.user_set.add(self.eleve.user) # On ajoute l'élève au groupe de permissions
         super(Adhesion, self).save(*args, **kwargs) # Sauvegarde
         
     def delete(self):
-        self.association.groupe_permissions.user_set.remove(self.eleve.user) #On retire l'eleve du groupe de permissions
+        self.association.groupe_permissions.user_set.remove(self.eleve.user) # On retire l'élève du groupe de permissions
         super(Adhesion, self).delete()
 
     @staticmethod
     def existe(eleve, association):
+        """
+            Renvoie vrai si l'élève est membre de l'association
+        """
         return Adhesion.objects.filter(association=association, eleve=eleve).exists()
-    
-# Video sur la page medias d'une assoce
-class Video(models.Model):
-    association = models.ForeignKey(Association, blank=True, null=True)
-    titre = models.CharField(max_length=64)
-    url  = models.URLField(max_length=200)
-    date = models.DateField(default=datetime.now)
-    
-    notification = generic.GenericRelation(Notification)
 
+class Video(models.Model):
+    """
+        Une vidéo sur la page 'médias' d'une association
+    """
+    association = models.ForeignKey(Association, blank=True, null=True)
+    titre = models.CharField(max_length=64, help_text="Titre de la vidéo")
+    url = models.URLField(max_length=200, help_text="Lien vers une vidéo Youtube ou Vimeo")
+    date = models.DateField(default=datetime.now, help_text="Date de publication")
+    notification = generic.GenericRelation(Notification)
 
     class Meta:
         ordering = ['-date', '-id']
@@ -89,14 +91,14 @@ class Video(models.Model):
         notification.envoyer_multiple(self.association.suivi_par.all())
         
     def save(self, *args, **kwargs):
-        creation = self.pk is None #Creation de l'objet
-        if self.url[:31] == 'http://www.youtube.com/watch?v=': # On reconnait un lien direct vers une video youtube
+        creation = self.pk is None # Création de l'objet
+        if self.url[:31] == 'http://www.youtube.com/watch?v=': # Lien direct vers une video youtube
             self.url = 'http://www.youtube.com/embed/' + self.url[31:].split('&')[0]
-        elif self.url[:29] == 'http://www.youtube.com/embed/': # On reconnait un lien embed vers une video youtube
+        elif self.url[:29] == 'http://www.youtube.com/embed/': # Lien embed vers une video youtube
             pass
-        elif self.url[:18] == 'https://vimeo.com/': # On reconnait un lien direct vers une video vimeo
+        elif self.url[:18] == 'https://vimeo.com/': # Lien direct vers une video vimeo
             self.url = 'http://player.vimeo.com/video/' + self.url[18:]
-        elif self.url[:30] == 'http://player.vimeo.com/video/':
+        elif self.url[:30] == 'http://player.vimeo.com/video/': # Lien embed vers une video vimeo
             pass
         else:
             self.url = ''
@@ -104,13 +106,14 @@ class Video(models.Model):
         if creation:            
             self.envoyer_notification()
         
-# Les affiches pour la page medias d'une assoce
 class Affiche(models.Model):
+    """
+        Une affiche sur la page "médias" d'une association
+    """
     association = models.ForeignKey(Association, blank=True, null=True)
     titre = models.CharField(verbose_name='Titre de l\'affiche', max_length=64)
     fichier = models.ImageField(upload_to = 'affiches')
     date = models.DateField(verbose_name='Date de publication', default=datetime.now, blank=True, null=True)
-
     notification = generic.GenericRelation(Notification)
     
     class Meta:
@@ -128,24 +131,29 @@ class Affiche(models.Model):
         notification.envoyer_multiple(self.association.suivi_par.all())
         
     def save(self, *args, **kwargs):
-        creation = self.pk is None #Creation de l'objet            
+        creation = self.pk is None # Création de l'objet
         super(Affiche, self).save(*args, **kwargs)
         if creation:            
             self.envoyer_notification()
-        
 
-#Les pages personalisées des associations. On ne précise que le titre de la page et son url. La page en elle même est à implémenter dans une application ad-hoc.
 class Page(models.Model):
+    """
+        Les pages personnalisées des associations. 
+        On ne précise ici que le titre de la page et son url. 
+        La page en elle-même est à implémenter dans une application séparée.
+    """
     association = models.ForeignKey(Association)
     titre = models.CharField(max_length=200)
-    lien = models.CharField(max_length=200)
-    is_externe = models.BooleanField(help_text="Lien vers un site externe au portail", verbose_name = "est externe") 
+    lien = models.SlugField(max_length=200, help_text="URL de la page. Ne donner que le nom de l'association si la page est interne.")
+    is_externe = models.BooleanField(help_text="Vrai si la page est un site externe au portail", verbose_name = "est externe") 
     
     def __unicode__(self):
         return self.association.nom + ' - ' + self.titre
 
-        
-#FORMULAIRES
+###################
+### FORMULAIRES ###
+###################
+
 class AdhesionAjoutForm(forms.Form):
 
     eleve = forms.ModelChoiceField(queryset=UserProfile.objects.all())
@@ -153,6 +161,7 @@ class AdhesionAjoutForm(forms.Form):
     
     def __init__(self, association, *args, **kwargs):
         super(AdhesionAjoutForm, self).__init__(*args, **kwargs)
+        ### On retire les membres actuels de la liste
         self.fields['eleve'].queryset = UserProfile.objects.exclude(id__in = [m.id for m in association.membres.all()])
         
 class AdhesionSuppressionForm(forms.Form):
