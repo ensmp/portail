045341ccc02041d7a1859acb7727d39ffb5baf40
Edouard Leurent <eleurent@gmail.com>
2014-02-26 20:19:49 +0100

    Removed todo and objet trouvés modules

--- objettrouve/admin.py
+++
@@ -1,4 +0,0 @@
-from objettrouve.models import ObjetTrouve 
-from django.contrib import admin
-
-admin.site.register(ObjetTrouve)
--- todo/admin.py
+++
@@ -1,4 +0,0 @@
-from todo.models import Todo
-from django.contrib import admin
-
-admin.site.register(Todo)
