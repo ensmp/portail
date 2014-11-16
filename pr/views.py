17b36c6e80f766a7fea3d155cd5e4156d73f17cd
13anjou <valentin.anjou@mines-paristech.fr>
2014-11-16 15:33:32 +0100

    Clips pr cachés aux 1As

--- pr/views.py
+++ pr/views.py
@@ -1,8 +1,17 @@
+#-*- coding: utf-8 -*-
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, get_object_or_404
 from pr.models import Clip
 from django.template import RequestContext
 
 
+@login_required
 def clips(request):
-	clip_list = Clip.objects.all()
+
+
+	if request.user.get_profile().en_premiere_annee():
+		clip_list = []
+	else :
+		clip_list = Clip.objects.all()
+		
 	return render_to_response('pr/clips.html', {'clip_list': clip_list}, context_instance=RequestContext(request))
