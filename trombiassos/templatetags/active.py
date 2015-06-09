49665a10c87de469f3c9bdcd331479d1c78ca3c5
MaximeBrt <maxime.brunet@mines-paristech.fr>
2015-06-09 23:26:03 +0200

    Revert "Add trombi with association at trombiassos/"
    This reverts commit cf111445f35f42febc0982df05aad9d22872720b.

--- trombiassos/templatetags/active.py
+++
@@ -1,9 +0,0 @@
-from django.template import Library
-register = Library()
-
-@register.simple_tag
-def active(request, pattern):
-    import re
-    if re.search(pattern, request.path):
-        return 'active'
-    return ''
