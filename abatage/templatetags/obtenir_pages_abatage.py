ff9b8701917d7812000b0bd93c261eba85ede444
MaximeBrt <maxime.brunet@mines-paristech.fr>
2015-06-23 19:22:31 +0200

    Modification pour le nouvel abatage

--- abatage/templatetags/obtenir_pages_abatage.py
+++ abatage/templatetags/obtenir_pages_abatage.py
@@ -1,43 +1,47 @@
-﻿# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 from django.template import Library, Node
 from association.models import Association
 from collections import OrderedDict
+from django import template
 
-register = Library()
+register = template.Library()
      
 class AbatagePageNode(Node):
     def render(self, context):
-        pages = {"bde": "15", 
-                 "bda": "18", 
-                 "bds": "31", 
-                 "mediamines": "19", 
-                 "vendome": "27", 
+        pages = {"bde": "30", 
+                 "bda": "34", 
+                 "bds": "32", 
+                 "mediamines": "40", 
+                 "vendome": "44", 
                  "minesmarket": "20", 
-                 "jump": "17", 
-                 "trium": "17", 
-                 "biero": "25", 
-                 "mineshake": "26", 
-                 "minestryofsound": "29", 
+                 "jump": "37", 
+                 "trium": "36", 
+                 "biero": "42", 
+                 "mineshake": "43", 
+                 "minotaure":"41",
+                 "minestryofsound": "47", 
                  "mds": "22", 
-                 "wei": "16", 
-                 "asti": "16", 
+                 "rezal":"40",
+                 "cahiervert":"39",
+                 "wei": "24", 
+                 "asti": "38", 
                  "pr": "27", 
-                 "rock": "28", 
-                 "cav": "25", 
+                 "rock": "47", 
+                 "cav": "43", 
                  "rezal": "19", 
                  "cahiervert": "18", 
-                 "vp": "13", 
-                 "cc": "19", 
-                 "fanfare": "26", 
-                 "voile": "28", 
-                 "skiclub": "28", 
+                 "vp": "28", 
+                 "cc": "54", 
+                 "fanfare": "46", 
+                 "voile": "62", 
+                 "skiclub": "63", 
                  "abatage": "27", 
                  "minotaure": "20", 
                  "handivalides": "20", 
                  "routes": "21", 
-                 "macadam": "22", 
+                 "macadam": "54", 
                  "mineStream": "29", 
-                 "glam": "19"}
+                 "glam": "51"}
         abatage_pages_associations = {Association.objects.get(pseudo=pseudo):page for pseudo, page in pages.iteritems()}
         abatage_pages_associations = OrderedDict(sorted(abatage_pages_associations.items(), key=lambda x: x[0].ordre)) # Pour classer par page, choisir key=lambda x: x[1]
         context['abatage_pages_associations'] = abatage_pages_associations
