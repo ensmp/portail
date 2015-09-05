cd6c3a0ab0415a9d1bf47e1376b2d46739acab95
MaximeBrt <maxime.brunet@mines-paristech.fr>
2015-09-05 15:34:03 +0200

    Revert "Modifications petits cours"
    This reverts commit 2806ca3487a4f81a72f7dc33c94c29f135e4ae2c.

--- petitscours/admin.py
+++ petitscours/admin.py
@@ -1,12 +1,4 @@
 from petitscours.models import PetitCours 
 from django.contrib import admin
 
-
-class PetitCoursAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date_added', 'visible', 'niveau', 'matiere', 'attribue_a')
-    list_filter = ['date_added', 'visible']
-    search_fields = ['title']
-    list_per_page = 100
-
-
-admin.site.register(PetitCours,PetitCoursAdmin)
+admin.site.register(PetitCours)
