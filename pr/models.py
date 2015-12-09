275224b78b05671420f1c2569a8de0965040a14f
giliam <giliam@users.noreply.github.com>
2015-12-09 20:50:44 +0100

    Corrige le bug de l'ajout de clip de l'app PR
    Mauvais import : vu qu'il s'agissait d'un from datetime import datetime, on a directement accès à today.

--- pr/models.py
+++ pr/models.py
@@ -12,7 +12,7 @@ from django.forms import ModelForm
 class Clip(models.Model):
 	titre = models.CharField(max_length=100)
 	lien = models.URLField()
-	promo = models.IntegerField(default=lambda : datetime.date.today().year-2002)
+	promo = models.IntegerField(default=lambda : datetime.today().year-2002)
 	
 	class Meta:
 		ordering = ["-promo", "titre"]
