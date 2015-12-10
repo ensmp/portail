aff8313be36b7d9a962f2459fe2be09c0e63287d
Sparadox <etienne.lafarge@gmail.com>
2015-12-10 10:53:46 -0500

    [bugfix] Fixed indentation issue

--- pr/models.py
+++ pr/models.py
@@ -10,29 +10,29 @@ from django import forms
 from django.forms import ModelForm
 
 class Clip(models.Model):
-	titre = models.CharField(max_length=100)
-	lien = models.URLField()
-	promo = models.IntegerField(default=lambda : datetime.today().year-2002)
-	
-	class Meta:
-		ordering = ["-promo", "titre"]
-		
-	def __unicode__(self):
-		return self.titre
-	
+    titre = models.CharField(max_length=100)
+    lien = models.URLField()
+    promo = models.IntegerField(default=lambda : datetime.today().year-2002)
 
-class Candidat(models.Model):    #C'est la petite zoe qui veut faire de la balancoire mais elle n'y arrive pas. 
-    nom = models.CharField(max_length=32)   #Pourquoi 
+    class Meta:
+        ordering = ["-promo", "titre"]
+
+    def __unicode__(self):
+        return self.titre
+
+
+class Candidat(models.Model):    #C'est la petite zoe qui veut faire de la balancoire mais elle n'y arrive pas.
+    nom = models.CharField(max_length=32)   #Pourquoi
     nbVotes = models.IntegerField(max_length=7) #Parce qu'elle n'a pas de bras ... merci 13Jougla
     debut_vote = models.DateTimeField(default=datetime.now)
     fin_vote = models.DateTimeField(default=datetime.now)
-    
+
     def __unicode__(self):
-        return self.nom  
+        return self.nom
 
 class Vote(models.Model):
     liste = models.ForeignKey(Candidat)
     eleve = models.ForeignKey(UserProfile, related_name="votes")
-    
+
     def __unicode__(self):
         return 'vote de ' + self.eleve.user.username + ' pour ' + self.liste.nom
